260a7553851b28bd3474aeee8dac46f6c9246b30
zafer hüzmeli <zaferhuzmeli@yahoo.com.tr>
2021-11-18 04:42:27 +0300

    forgotten one more letter

--- todo-list/src/components/Todo.js
+++ todo-list/src/components/Todo.js
@@ -23,7 +23,7 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     }
 
     return todos.map((todo, index) => (
-        <div className={todo.isCompleted ? 'todo-row complete' : 'todo-row'} key={index}>
+        <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
 
             <div key={todo.id} onClick={() => completeTodo(todo.id)}> {todo.text} </div>
             <div className="icons">
